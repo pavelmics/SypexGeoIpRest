ff777c467699b3d812d312c93f396d37c4be8fc4
Pavel Mikushin <pavelmics@gmail.com>
2015-05-25 11:59:42 +0600

    first one

---
+++ src/SypixGeoRest.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Class SypixGeoRest
+ *
+ * @link https://sypexgeo.net/ru/api/
+ */
+class SypixGeoRest
+{
+	/**
+	 * Sypex JSON API url
+	 * @var string
+	 */
+	protected $_apiUrl = 'http://api.sypexgeo.net/';
+
+	/**
+	 * Key for REST api.
+	 * You can get your key in your account information at @link http://sypexgeo.net/
+	 * @var string|null
+	 */
+	protected $_apiKey = null;
+
+	/**
+	 * @var \Guzzle\Http\Client
+	 */
+	protected $_client;
+
+	/**
+	 * Last error
+	 * @var bool|array
+	 */
+	public $lastError = false;
+
+	/**
+	 * @constructor
+	 * @param array $params
+	 */
+	public function __construct(array $params = [])
+	{
+		$this->_client = new GuzzleHttp\Client();
+		isset($params['key'])
+			? $this->_apiKey = $params['key']
+			: null;
+	}
+
+	/**
+	 * Returns data provided by sypexgeo service
+	 *
+	 * @param $ip
+	 * @return array
+	 */
+	public function get($ip)
+	{
+		$url = $this->_getApiUrl() . $ip;
+		$jsonResult = $this->_req($url);
+
+		return $jsonResult
+			? json_decode($jsonResult, true)
+			: false;
+	}
+
+	/**
+	 * Builds API url
+	 *
+	 * @return string
+	 */
+	protected function _getApiUrl()
+	{
+		empty($this->_apiKey)
+			? $path = 'json/'
+			: $path = $this->_apiKey . '/json/';
+
+		return $this->_apiUrl . $path;
+	}
+
+	/**
+	 * Makes http GET request
+	 *
+	 * @param $url
+	 * @return string|bool
+	 */
+	protected function _req($url)
+	{
+		try {
+			$result = $this->_client->get($url);
+		} catch (GuzzleHttp\Exception\TransferException $e) {
+			$result = false;
+			$this->lastError = [
+				'errorType' => 'http_client_error',
+				'request' => method_exists($e, 'getRequest') ? $e->getRequest() : '',
+			];
+		}
+
+		return $result->getBody();
+	}
+}
